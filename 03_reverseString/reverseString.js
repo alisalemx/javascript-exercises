b22a299edb2bb780876a267e23bae8ec55b6c76f
Ali Salem <1208400+alisalemx@users.noreply.github.com>
2024-12-12 01:03:05 -0500

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let chars = string.split("");
+  let reversedChars = chars.reverse();
 
+  return reversedChars.join("");
 };
 
 // Do not edit below this line
