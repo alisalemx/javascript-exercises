2f63bd094f338a5506ec1d34cdc62d50dfda57c4
Ali Salem <1208400+alisalemx@users.noreply.github.com>
2024-12-13 10:57:23 -0500

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  let trimmedText = text
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, '');
+  let reversedText = trimmedText
+    .split('')
+    .reverse()
+    .join('');
 
+  return trimmedText === reversedText;
 };
 
 // Do not edit below this line
