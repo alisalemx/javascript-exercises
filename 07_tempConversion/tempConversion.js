5169766c0ce5958f425190546bd185272eb72338
Ali Salem <1208400+alisalemx@users.noreply.github.com>
2024-12-13 09:50:27 -0500

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemp) {
+  const celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
+
+  // Round to one decimal place
+  return Math.round(celsiusTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusTemp) {
+  const fahrenheitTemp = (celsiusTemp * 9 / 5) + 32;
+
+  // Round to one decimal place
+  return Math.round(fahrenheitTemp * 10) / 10;
 };
 
 // Do not edit below this line
