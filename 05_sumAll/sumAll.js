4698839fc1e3202d7a85d141cc18a2d0c0eca140
Ali Salem <1208400+alisalemx@users.noreply.github.com>
2024-12-13 00:42:04 -0500

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+  // Detect invalid inputs early on
+  if (
+    !Number.isInteger(first) ||
+    !Number.isInteger(last) ||
+    first < 0 ||
+    last < 0
+  ) {
+    return "ERROR";
+  }
 
+  // Swap first and last if first is bigger
+  if (first > last) {
+    const temp = first;
+    first = last;
+    last = temp;
+  }
+
+  const iterations = last - first;
+  let sum = first;
+
+  for (let i = 1; i <= iterations; i++) {
+    sum += (first + i);
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
