90938359e9f2dd54c85555fdeeb669a7f4d566fc
Ali Salem <1208400+alisalemx@users.noreply.github.com>
2024-12-12 20:13:09 -0500

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  for (const arg of args) { // loop through the args
+    while (array.includes(arg)) { // loop through the array
+      let index = array.indexOf(arg); // get the index of the arg
+      array.splice(index, 1); // remove the arg based on its index
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
