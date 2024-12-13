469c8cd5f8680147713dc3e06c35ce1d5a136501
Ali Salem <1208400+alisalemx@users.noreply.github.com>
2024-12-13 01:01:22 -0500

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      // False if divisible by 100
+      // True if divisible by 100 and 400
+      return year % 400 === 0;
+    }
+    // True if divisible by 4
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
