fb1d32db1e1cabfc519270ecc9d907cd4435798a
Ali Salem <1208400+alisalemx@users.noreply.github.com>
2024-12-17 09:22:32 -0500

    Solve 12_findTheOldest after looking at the solution to better understand the reduce() method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
 
+    return currentAge > oldestAge ? currentPerson : oldestPerson;
+  });
 };
+
+const getAge = function (birthYear, deathYear) {
+  if (!deathYear) {
+    deathYear = new Date().getFullYear();
+  }
+
+  return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
